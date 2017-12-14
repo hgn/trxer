5184e02c37aeec58a309a0171bc8d28314654384
Hagen Paul Pfeifer <hagen@jauu.net>
2017-12-14 21:20:30 +0100

    add UDP server socket
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- trxer.go
+++ trxer.go
@@ -9,15 +9,72 @@ import "strconv"
 import "sync"
 
 var UPDATE_INTERVAL = 5
-// 32k
+
 var BYTE_BUFFER_SIZE = 8096 * 8
+
+type measurement struct {
+	bytes uint64
+	time  float64
+}
 
 func udp_client(threads int, addr string) {
 	//
 }
 
+func udp_server_worker(c chan<- measurement, port int) {
+
+	listen := "[::]:" + strconv.Itoa(port);
+	println("Listening on", listen)
+	pc, error := net.ListenPacket("udp", listen);
+	if error != nil {
+		fmt.Printf("Cannot listen: %s\n", error);
+		os.Exit(1);
+	}
+	defer pc.Close()
+
+	message := make([]byte, BYTE_BUFFER_SIZE, BYTE_BUFFER_SIZE);
+
+	var bytes uint64  = 0;
+	start := time.Now()
+	for {
+		read, _, error := pc.ReadFrom(message);
+		if error != nil {
+			fmt.Printf("Cannot read: %s\n", error);
+			os.Exit(1);
+		}
+
+		bytes += uint64(read)
+
+		elapsed := time.Since(start)
+		if elapsed.Seconds() > float64(UPDATE_INTERVAL) {
+			c <- measurement{bytes: bytes, time: elapsed.Seconds()}
+			start = time.Now()
+			bytes = 0
+		}
+	}
+
+}
+
+
 func udp_server(threads int) {
-	//
+	c := make(chan measurement)
+	port := 6666
+	for i := 0; i < threads; i++ {
+		go udp_server_worker(c, port)
+		port += 1
+	}
+
+
+	var accumulated uint64  = 0;
+	for {
+		for i := 0; i < threads; i++ {
+			val := <-c
+			accumulated += val.bytes
+		}
+		mbyte_sec := accumulated / (1000000 * uint64(UPDATE_INTERVAL))
+		println("MByte/sec: ", mbyte_sec)
+		accumulated = 0
+	}
 }
 
 func tcp_client_worker(addr string, wg sync.WaitGroup) {
@@ -48,13 +105,7 @@ func tcp_client(threads int, addr string) {
 	wg.Wait()
 }
 
-type measurement struct {
-	bytes uint64
-	time  float64
-}
-
 func tcp_server(threads int) {
-
 	c := make(chan measurement)
 	port := 6666
 	for i := 0; i < threads; i++ {
@@ -77,8 +128,8 @@ func tcp_server(threads int) {
 
 func tcp_server_worker(c chan<- measurement, port int) {
 
-	listen := "localhost:" + strconv.Itoa(port);
-	println("listening on ", listen)
+	listen := "[::]:" + strconv.Itoa(port);
+	println("Listening on", listen)
 	addr, error := net.ResolveTCPAddr("tcp", listen);
 	if error != nil {
 		fmt.Printf("Cannot parse \"%s\": %s\n", listen, error);
@@ -127,9 +178,10 @@ func main() {
     threadPtr := flag.Int("threads", 1, "an int for numer of coroutines")
 
     flag.Parse()
-    fmt.Println("protocol: ", *protoPtr)
-    fmt.Println("mode:     ", *modePtr)
-    fmt.Println("threads:  ", *threadPtr)
+    fmt.Println("trxer(c) - 2017")
+    fmt.Println("Protocol:", *protoPtr)
+    fmt.Println("Mode:", *modePtr)
+    fmt.Println("Threads:", *threadPtr)
 
     if (*protoPtr == "udp") {
 	    if (*modePtr == "server") {
